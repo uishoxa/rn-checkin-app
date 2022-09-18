7f3c63f4c0a0d602218f1aad1b5eda5d0a041aca
Shoxa <bapeshoxa@gmail.com>
2022-09-18 20:40:06 +0500

    Create Test.js
    Test js

---
+++ components/Test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import react from "react";
+import { View } from "react-native";
+
+
+class Test extends React.Component{
+
+    render(){
+        return(
+            <View>
+                <Text>Hello world</Text>
+            </View>
+        )
+    }
+}
+
+export default Test
